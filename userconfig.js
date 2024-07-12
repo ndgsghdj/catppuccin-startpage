aaf77afe847b7954460a222c9b554b948e1a56b1
ndgsghdj <75611248+ndgsghdj@users.noreply.github.com>
2024-07-12 14:37:13 +0800

    remove changes

--- userconfig.js
+++ userconfig.js
@@ -64,6 +64,41 @@ const default_config = {
             },
           ],
         },
+        {
+          name: "media",
+          links: [
+            {
+              name: "уп",
+              url: "https://www.pravda.com.ua",
+              icon: "news",
+              icon_color: "#a6e3a1",
+            },
+            {
+              name: "mil.in.ua",
+              url: "https://mil.in.ua",
+              icon: "badge-filled",
+              icon_color: "#fab387",
+            },
+            {
+              name: "куток",
+              url: "https://kutok.io",
+              icon: "border-radius",
+              icon_color: "#f38ba8",
+            },
+            {
+              name: "ґрунт",
+              url: "https://grnt.media",
+              icon: "eye-bolt",
+              icon_color: "#89b4fa",
+            },
+            {
+              name: "village",
+              url: "https://www.village.com.ua",
+              icon: "home-2",
+              icon_color: "#cba6f7",
+            },
+          ],
+        },
       ],
     },
     {
@@ -94,6 +129,57 @@ const default_config = {
           ],
         },
         {
+          name: "challenges",
+          links: [
+            {
+              name: "kaggle",
+              url: "https://www.kaggle.com/volodymyrpivoshenko",
+              icon: "brain",
+              icon_color: "#a6e3a1",
+            },
+            {
+              name: "leetcode",
+              url: "https://leetcode.com",
+              icon: "code-plus",
+              icon_color: "#fab387",
+            },
+            {
+              name: "stackoverflow",
+              url: "https://stackoverflow.com",
+              icon: "brand-stackoverflow",
+              icon_color: "#f38ba8",
+            },
+          ],
+        },
+        {
+          name: "blogs",
+          links: [
+            {
+              name: "dou",
+              url: "https://dou.ua",
+              icon: "brand-prisma",
+              icon_color: "#a6e3a1",
+            },
+            {
+              name: "hackernews",
+              url: "https://news.ycombinator.com",
+              icon: "brand-redhat",
+              icon_color: "#fab387",
+            },
+            {
+              name: "uber research",
+              url: "https://eng.uber.com/category/articles",
+              icon: "brand-uber",
+              icon_color: "#f38ba8",
+            },
+            {
+              name: "google research",
+              url: "https://blog.research.google",
+              icon: "hexagon-letter-g",
+              icon_color: "#89b4fa",
+            },
+          ],
+        },
       ],
     },
     {
@@ -120,6 +206,64 @@ const default_config = {
               url: "https://www.reddit.com/r/unixporn",
               icon: "brand-reddit",
               icon_color: "#f38ba8",
+            },
+          ],
+        },
+        {
+          name: "games",
+          links: [
+            {
+              name: "GG",
+              url: "https://ggapp.io/pivoshenko",
+              icon: "device-gamepad",
+              icon_color: "#a6e3a1",
+            },
+            {
+              name: "steam",
+              url: "https://store.steampowered.com",
+              icon: "brand-steam",
+              icon_color: "#fab387",
+            },
+            {
+              name: "epicgames",
+              url: "https://store.epicgames.com",
+              icon: "brand-fortnite",
+              icon_color: "#f38ba8",
+            },
+            {
+              name: "nintendo",
+              url: "https://store.nintendo.co.uk",
+              icon: "device-nintendo",
+              icon_color: "#89b4fa",
+            },
+          ],
+        },
+        {
+          name: "video",
+          links: [
+            {
+              name: "anilist",
+              url: "https://anilist.co/home",
+              icon: "brand-funimation",
+              icon_color: "#a6e3a1",
+            },
+            {
+              name: "youtube",
+              url: "https://www.youtube.com",
+              icon: "brand-youtube",
+              icon_color: "#fab387",
+            },
+            {
+              name: "patreon",
+              url: "https://www.patreon.com",
+              icon: "brand-patreon",
+              icon_color: "#f38ba8",
+            },
+            {
+              name: "kyivstar",
+              url: "https://tv.kyivstar.ua",
+              icon: "star-filled",
+              icon_color: "#89b4fa",
             },
           ],
         },
